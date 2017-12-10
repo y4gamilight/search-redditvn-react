3547c0304a1887c525fe7252b3a31ac233bdaa33
sontrinh <sontrinh.ns@gmail.com>
2017-12-10 17:24:07 +0700

    Fix no paginate when no post

--- src/containers/User.js
+++ src/containers/User.js
@@ -52,7 +52,7 @@ class User extends Component {
 
     const currentPage = parseInt(this.props.userPosts.page - 1, 10);
 
-    const pagnite = this.props.posts.docs.length > 0 ? (
+    const pagnite = this.props.userPosts.docs.length > 0 ? (
       <div className="nav justify-content-end">
         <CustomPaginate currentPage={currentPage} totalPage={this.props.userPosts.pages} onPageChange={this.onPageChange} />
       </div>
